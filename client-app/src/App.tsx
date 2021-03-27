6d5a931a3cdbbdfaea6d0a468b8ad7a62ffa0bd9
Austin Webre <austin.webre@gmail.com>
2021-03-26 20:39:22 -0500

    styles!

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,14 +7,12 @@ import { useDebounce } from "./hooks";
 
 export default function App() {
   //TODO: move this into a component (called something like MarkersGeoSearch)
-  const [location, setLocation] = useState({
+  const [region, setRegion] = useState({
     latitude: 30.9843,
     longitude: -91.9623,
     latitudeDelta: 5,
     longitudeDelta: 5,
   });
-
-  const [region, setRegion] = useState(location);
   const debouncedRegion = useDebounce(region, 500);
 
   useEffect(() => {
@@ -25,7 +23,7 @@ export default function App() {
       }
 
       const { coords } = await Location.getCurrentPositionAsync({});
-      setLocation({ latitudeDelta: 0.1, longitudeDelta: 0.1, ...coords });
+      setRegion({ latitudeDelta: 0.1, longitudeDelta: 0.1, ...coords });
     };
 
     setUserLocation();
@@ -36,7 +34,7 @@ export default function App() {
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        initialRegion={location}
+        region={region}
         showsUserLocation={true}
         onRegionChange={setRegion}
       >
@@ -51,9 +49,35 @@ export default function App() {
             />
           ))}
       </MapView>
-      {!isLoading &&
-        !isError &&
-        markers?.map((m) => <Text key={m.Id}>{m.Name}</Text>)}
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardHeaderText}>Nearby Historical Markers</Text>
+        </View>
+        {!isLoading &&
+          !isError &&
+          markers?.map((m, index) => (
+            <View
+              key={m.Id}
+              style={
+                index + 1 < markers.length
+                  ? styles.listItemWithBorder
+                  : styles.listItem
+              }
+            >
+              <Text>{m.Name}</Text>
+            </View>
+          ))}
+        {isLoading && (
+          <View style={styles.listItem}>
+            <Text>Looking for nearby markers...</Text>
+          </View>
+        )}
+        {!isLoading && (!markers || markers.length === 0) && (
+          <View style={styles.listItem}>
+            <Text>No markers found</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 }
@@ -61,9 +85,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#ecd9c6",
     alignItems: "center",
     justifyContent: "flex-start",
+  },
+  card: {
+    borderRadius: 5,
+    backgroundColor: "#f9f2ec",
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+    margin: 20,
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowRadius: 1,
+    shadowOffset: { width: 1, height: 1 },
+  },
+  cardHeader: {
+    backgroundColor: "#996633",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    padding: 10,
+  },
+  cardHeaderText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  listItemWithBorder: {
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
   },
   map: {
     width: Dimensions.get("window").width,
