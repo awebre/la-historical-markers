53c3b8779e7e384636305a518e3178876767b7ec
Austin Webre <austin.webre@gmail.com>
2021-04-10 12:36:48 -0500

    version bump for 1.1 release

--- client-app/app.config.ts
+++ client-app/app.config.ts
@@ -4,13 +4,13 @@ export default {
   expo: {
     name: "LA Historical Markers",
     slug: "la-historical-markers",
-    version: "1.0.0",
+    version: "1.1.0",
     assetBundlePatterns: ["**/*"],
     orientation: "portrait",
     icon: "./icon.png",
     ios: {
       bundleIdentifier: "com.austinwebre.lahistoricalmarkers",
-      buildNumber: "1.0.4",
+      buildNumber: "1.1.0",
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
           "This app uses your location to search for nearby markers.",
@@ -20,7 +20,7 @@ export default {
     },
     android: {
       package: "com.austinwebre.lahistoricalmarkers",
-      versionCode: 5,
+      versionCode: 6,
       permissions: ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
       config: {
         googleMaps: {
