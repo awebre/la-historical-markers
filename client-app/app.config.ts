64c8bcfc831bc479bd0056eaef224a07bbdb522b
Austin Webre <austin.webre@gmail.com>
2021-04-16 00:10:46 -0500

    fix android adaptive icon

--- client-app/app.config.ts
+++ client-app/app.config.ts
@@ -1,5 +1,5 @@
 //increase this for build version bumps (internal to app stores)
-const buildNumber = 14;
+const buildNumber = 15;
 export default {
   name: "client-app",
   displayName: "LA Markers",
