a49b92a06f5f12657824109bc2e560a505f85e71
Austin Webre <austin.webre@gmail.com>
2021-06-27 23:53:29 -0500

    wip: idea for stats

--- client-app/src/screens/MyMarkersScreen.tsx
+++ client-app/src/screens/MyMarkersScreen.tsx
@@ -1,9 +1,9 @@
 import { getMarkerColor } from "components/markers/utils";
 import { useSavedMarkers } from "hooks";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import { Locations } from "utils";
+import { colors, Locations } from "utils";
 
 const initialRegion = {
   ...Locations.center,
@@ -14,31 +14,49 @@ export default function MyMarkersScreen() {
   const { markers } = useSavedMarkers();
   console.log(markers);
   return (
-    <MapView
-      style={styles.map}
-      showsUserLocation={true}
-      initialRegion={initialRegion}
-    >
-      {markers?.map((m) => {
-        return (
-          <Marker
-            key={m.id.toString()}
-            identifier={m.id.toString()}
-            coordinate={{
-              latitude: m.latitude,
-              longitude: m.longitude,
-            }}
-            title={m.name}
-            pinColor={getMarkerColor(m.type)}
-          />
-        );
-      })}
-    </MapView>
+    <>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        initialRegion={initialRegion}
+      >
+        {markers?.map((m) => {
+          return (
+            <Marker
+              key={m.id.toString()}
+              identifier={m.id.toString()}
+              coordinate={{
+                latitude: m.latitude,
+                longitude: m.longitude,
+              }}
+              title={m.name}
+              pinColor={getMarkerColor(m.type)}
+            />
+          );
+        })}
+      </MapView>
+      <View style={styles.statsContainer}>
+        <View style={styles.stat}>
+          <Text style={styles.stat}>{`${markers.length} markers saved`}</Text>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   map: {
-    height: "100%",
+    height: "90%",
+  },
+  statsContainer: {
+    height: "10%",
+    backgroundColor: colors.mediumBackground,
+  },
+  stat: {
+    borderRadius: 5,
+    margin: 10,
+    fontSize: 24,
+    color: colors.black,
+    backgroundColor: colors.lightBackground,
   },
 });
