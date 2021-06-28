d2a3d1c0711615f04354d58d522c0f18d913a105
Austin Webre <austin.webre@gmail.com>
2021-06-27 19:13:49 -0500

    chore: bump version

--- client-app/app.config.ts
+++ client-app/app.config.ts
@@ -1,5 +1,5 @@
 //increase this for build version bumps (internal to app stores)
-const buildNumber = 30;
+const buildNumber = 31;
 export default {
   name: "client-app",
   displayName: "LA Markers",
